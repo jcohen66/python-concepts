d708eec6ae2a6f2c811eefb2f1e7f03d2f9dee74
Jonathan T. Cohen <jcohen66g@gmail.com>
2023-01-09 22:42:04 -0500

    updated sqs-wrapper

--- concepts/sqs-wrapper.py
+++ concepts/sqs-wrapper.py
@@ -27,6 +27,30 @@ class SQSWrapper:
             return json.loads(body)
         return None
 
+
+def pop_json_with_backoff(self):
+        # Initialize the backoff delay to 0
+        backoff_delay = 0
+
+        while True:
+            response = self.sqs.receive_message(
+                QueueUrl=self.queue_url,
+                MaxNumberOfMessages=1
+            )
+            if 'Messages' in response:
+                message = response['Messages'][0]
+                body = message['Body']
+                self.sqs.delete_message(
+                    QueueUrl=self.queue_url,
+                    ReceiptHandle=message['ReceiptHandle']
+                )
+                return json.loads(body)
+            else:
+                # If there are no messages, wait for the backoff delay before trying again
+                time.sleep(backoff_delay)
+                # Increase the backoff delay for the next iteration
+                backoff_delay = min(backoff_delay + 1, 32)
+
     def peek(self):
         response = self.sqs.receive_message(
             QueueUrl=self.queue_url,
